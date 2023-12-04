a3f22e81106b3935de7a91b59639eeb46a25c324
Roberto Mier Escandon <roberto.mierescandon@telefonica.com>
2023-12-04 21:30:28 +0100

    hard: prepare code to coexist extension and cli
    Signed-off-by: Roberto Mier Escandon <roberto.mierescandon@telefonica.com>

---
+++ src/core.ts
@@ -0,0 +1,136 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { ISettings } from "./settings";
+
+import { Strings } from "./utils";
+import { Line, LineFactory, Lines } from "./line";
+import { Table, TableLine } from "./table";
+
+export function buildDocument(
+  docLines: string[],
+  editBuilder: vscode.TextEditorEdit,
+  settings: ISettings
+) {
+  // tagLines is an array of consecutive lines starting with a tag. When the next line to the last with tag
+  // switches to a padded line, all them take the same padding. When that next line is not padded, the array
+  // is is emptied
+  let tagLines: Line[] = [];
+
+  // tab represents a table. It is populated with the header and rows of the table whilst identified on the
+  // document during consecutive lines.
+  // First line is considered the header of the table and calculates the number of row cells, the headers, and
+  // initialices the length of every cell.
+  // Second and next lines are validated against number of cells and the length of every cell is now recalculated.
+  // When reached a line not starting with |, if everything is valid, all table rows are updated
+  let tab = new Table();
+  // padding for the table is taken from paddingTable setting.
+  // When not set paddingTable it is taken from paddingDefault.
+  // and finally not set (set to -1) if none of the above is set
+  let tabPadding =
+    settings.paddingTable > 0
+      ? settings.paddingTable
+      : settings.paddingDefault > 0
+      ? settings.paddingDefault
+      : -1;
+
+  let emptyLines = new Lines();
+
+  docLines.forEach((lineText, idx) => {
+    let line = LineFactory.create(lineText, idx);
+
+    // address table lines
+    if (tabPadding >= 0) {
+      if (line.isTableLine()) {
+        if (tab.empty()) {
+          tab = new Table();
+        }
+        tab.push(new TableLine(line));
+        return;
+      }
+
+      if (!tab.empty() && tab.valid) {
+        const paddingStr = settings.paddingSymbol.repeat(tabPadding);
+        tab.update(paddingStr, editBuilder);
+        tab.reset();
+      }
+    }
+
+    // if empty line, reset tagLines array, draw table if not empty and continue
+    if (line.isEmpty()) {
+      tagLines = [];
+
+      // treat the blank line according to the settings, either removing its padding or leaving one
+      if (settings.consecutiveBlankLinesToOne) {
+        emptyLines.append(line);
+      } else {
+        line.updatePadding("", editBuilder);
+      }
+      return;
+    }
+
+    // if not empty line and list of empty lines is not empty, reduce all of them to a single one
+    if (!emptyLines.isEmpty()) {
+      emptyLines.updateContent("", editBuilder);
+      emptyLines.reset();
+    }
+
+    // if current line starts with @, append its number of line to the array of tag lines until finding
+    // an empty line or a line matching any defined keyword
+    if (line.isTagLine()) {
+      tagLines.push(line);
+      return;
+    }
+
+    // find a a keyword that matches the line initial string
+    let p = settings.paddings.find((padding) => {
+      if (padding) {
+        if (line.isKeywordLine(padding.keyword)) {
+          // normalize the line content to have exactly one space after the keyword
+          line.updateContent(
+            Strings.normalizeToJustOneSpaceAfterPrefix(
+              line.content,
+              padding.keyword
+            ),
+            editBuilder
+          );
+          return padding;
+        }
+      }
+      return undefined;
+    });
+
+    // use default padding when not found a keywork padding
+    let padding = settings.paddingDefault;
+    if (p) {
+      padding = p.padding;
+    }
+
+    // skip line if not found a either a keyword or a default padding
+    if (padding === -1) {
+      return;
+    }
+
+    // when found a padding to apply, build the padding string and update the line
+    const paddingStr = settings.paddingSymbol.repeat(padding);
+    line.updatePadding(paddingStr, editBuilder);
+
+    // update any cumulated tag lines and reset them
+    tagLines.forEach((line) => line.updatePadding(paddingStr, editBuilder));
+
+    if (settings.fixtureLineBreak) {
+      // insert a line break just before any @fixture found in tag line that is not at the beginning
+      tagLines.forEach((line) =>
+        line.updateContent(
+          line.content.replace(
+            /[^^]\@fixture/gi,
+            "\r\n" + paddingStr + "@fixture"
+          ),
+          editBuilder
+        )
+      );
+    }
+
+    tagLines = [];
+  });
+}
--- src/line.ts
+++ src/line.ts
@@ -1,140 +1,133 @@
-import * as vscode from 'vscode';
-import { Separators } from './utils';
-
+import * as vscode from "vscode";
+import { Separators } from "./utils";
 
 export interface ILine {
-    readonly pos: number
-    readonly indent: number
-    readonly content: string
+  readonly pos: number;
+  readonly indent: number;
+  readonly content: string;
 
-    isEmpty(): boolean
+  isEmpty(): boolean;
 
-    isTagLine(): boolean
-    isKeywordLine(keyword: string): boolean
-    isTableLine(): boolean
+  isTagLine(): boolean;
+  isKeywordLine(keyword: string): boolean;
+  isTableLine(): boolean;
 
-    updatePadding(paddingStr: string, editBuilder: vscode.TextEditorEdit): void
-    updateContent(newContent: string, editBuilder: vscode.TextEditorEdit): void
+  updatePadding(paddingStr: string, editBuilder: vscode.TextEditorEdit): void;
+  updateContent(newContent: string, editBuilder: vscode.TextEditorEdit): void;
 }
 
 export class LineFactory {
-    static create(pos: number, doc: vscode.TextDocument): ILine {
-        let line = doc.lineAt(pos);
-
-        let content = line.text.trimLeft();
-        let indent = line.text.length - content.length;
-        return new Line(pos, indent, content);
-    }
+  static create(text: string, pos: number): ILine {
+    let content = text.trimLeft();
+    let indent = text.length - content.length;
+    return new Line(pos, indent, content);
+  }
 }
 
 export class Line implements ILine {
-    readonly pos: number;
-    readonly indent: number;
-    readonly content: string;
+  readonly pos: number;
+  readonly indent: number;
+  readonly content: string;
 
-    constructor(pos: number, indent: number, content: string) {
-        this.pos = pos;
-        this.indent = indent;
-        this.content = content;
+  constructor(pos: number, indent: number, content: string) {
+    this.pos = pos;
+    this.indent = indent;
+    this.content = content;
+  }
+
+  // update a document line with a certain padding
+  updatePadding(paddingStr: string, editBuilder: vscode.TextEditorEdit) {
+    if (this.indent < 0) {
+      return;
+    }
+    let startPos = new vscode.Position(this.pos, 0);
+    let endPos = new vscode.Position(this.pos, this.indent);
+    let range = new vscode.Range(startPos, endPos);
+    editBuilder.replace(range, paddingStr);
+  }
+
+  updateContent(newContent: string, editBuilder: vscode.TextEditorEdit) {
+    if (this.indent < 0) {
+      return;
+    }
+    let startPos = new vscode.Position(this.pos, this.indent);
+    let endPos = new vscode.Position(
+      this.pos,
+      this.indent + this.content.length
+    );
+    let range = new vscode.Range(startPos, endPos);
+    editBuilder.replace(range, newContent);
+  }
+
+  isEmpty(): boolean {
+    return this.content.trim().length === 0;
+  }
+
+  isTagLine(): boolean {
+    if (!this.content) {
+      return false;
+    }
+    if (this.content.length === 0) {
+      return false;
+    }
+    return this.content.charAt(0) === "@";
+  }
+
+  isKeywordLine(keyword: string): boolean {
+    if (!keyword) {
+      return false;
+    }
+    if (!this.content) {
+      return false;
+    }
+    if (this.content.length === 0) {
+      return false;
+    }
+    return this.content.startsWith(keyword);
+  }
+
+  isTableLine(): boolean {
+    if (this.isEmpty()) {
+      return false;
+    }
+    if (this.content.length < 2) {
+      return false;
+    }
+    if (!Separators.validBoundaries(this.content)) {
+      return false;
+    }
+    if (Separators.count(this.content) < 2) {
+      return false;
     }
 
-    // update a document line with a certain padding
-    updatePadding(paddingStr: string, editBuilder: vscode.TextEditorEdit) {
-        if (this.indent < 0) {
-            return;
-        }
-        let startPos = new vscode.Position(this.pos, 0);
-        let endPos = new vscode.Position(this.pos, this.indent);
-        let range = new vscode.Range(startPos, endPos);
-        editBuilder.replace(
-            range,
-            paddingStr,
-        );
-    }
-
-    updateContent(newContent: string, editBuilder: vscode.TextEditorEdit) {
-        if (this.indent < 0) {
-            return;
-        }
-        let startPos = new vscode.Position(this.pos, this.indent);
-        let endPos = new vscode.Position(this.pos, this.indent + this.content.length);
-        let range = new vscode.Range(startPos, endPos);
-        editBuilder.replace(
-            range,
-            newContent,
-        );
-    }
-
-    isEmpty(): boolean {
-        return this.content.trim().length === 0;
-    }
-
-    isTagLine(): boolean {
-        if (!this.content) {
-            return false;
-        }
-        if (this.content.length === 0) {
-            return false;
-        }
-        return this.content.charAt(0) === '@';
-    }
-
-    isKeywordLine(keyword: string): boolean {
-        if (!keyword) {
-            return false;
-        }
-        if (!this.content) {
-            return false;
-        }
-        if (this.content.length === 0) {
-            return false;
-        }
-        return this.content.startsWith(keyword);
-    }
-
-    isTableLine(): boolean {
-        if (this.isEmpty()) {
-            return false;
-        }
-        if (this.content.length < 2) {
-            return false;
-        }
-        if (!Separators.validBoundaries(this.content)) {
-            return false;
-        }
-        if (Separators.count(this.content) < 2) {
-            return false;
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
 
-
 export class Lines {
-    lines: ILine[] = [];
+  lines: ILine[] = [];
 
-    append(line: ILine) {
-        this.lines.push(line);
-    }
+  append(line: ILine) {
+    this.lines.push(line);
+  }
 
-    isEmpty(): boolean {
-        return this.lines.length === 0;
-    }
+  isEmpty(): boolean {
+    return this.lines.length === 0;
+  }
 
-    reset() {
-        this.lines = [];
-    }
+  reset() {
+    this.lines = [];
+  }
 
-    updateContent(newContent: string, editBuilder: vscode.TextEditorEdit) {
-        let startLine = this.lines[0];
-        let endLine = this.lines[this.lines.length - 1];
-        let startPos = new vscode.Position(startLine.pos, 0);
-        let endPos = new vscode.Position(endLine.pos, endLine.indent + endLine.content.length);
-        let range = new vscode.Range(startPos, endPos);
-        editBuilder.replace(
-            range,
-            newContent,
-        );
-    }
+  updateContent(newContent: string, editBuilder: vscode.TextEditorEdit) {
+    let startLine = this.lines[0];
+    let endLine = this.lines[this.lines.length - 1];
+    let startPos = new vscode.Position(startLine.pos, 0);
+    let endPos = new vscode.Position(
+      endLine.pos,
+      endLine.indent + endLine.content.length
+    );
+    let range = new vscode.Range(startPos, endPos);
+    editBuilder.replace(range, newContent);
+  }
 }
